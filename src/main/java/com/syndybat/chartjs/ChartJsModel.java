5992a9b1600464fbbc7b1304e6104f07a54aebde
Jochec, Tomas <tjochec@netsuite.com>
2018-11-22 09:17:23 +0100

    fixed responsiveness in Chrome browser

--- src/main/java/com/syndybat/chartjs/ChartJsModel.java
+++ src/main/java/com/syndybat/chartjs/ChartJsModel.java
@@ -1,7 +1,6 @@
 package com.syndybat.chartjs;
 
 import com.vaadin.flow.templatemodel.TemplateModel;
-import elemental.json.JsonObject;
 
 public interface ChartJsModel extends TemplateModel {
     void setChartJs(String jsonChart);
